6b639c400961e29be2f91fcd2a42926bd8f02be1
Масальских Иван Сергеевич <imasalskikh@stream.ru>
2020-07-29 22:45:16 +0500

    Create project

---
+++ VkNewsFeed/VkNewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VkNewsFeed
+//
+//  Created by Иван Масальских on 29.07.2020.
+//  Copyright © 2020 Masalskikh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
